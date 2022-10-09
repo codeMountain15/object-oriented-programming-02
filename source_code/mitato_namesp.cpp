6af414d2578bac8ba4e4f3642189a69b34b6f2f9
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-09 18:50:11 +0300

    Create mitato_namesp.cpp

---
+++ source_code/mitato_namesp.cpp
@@ -0,0 +1,16 @@
+// mitato_namesp.cpp : This file is my second C++ program
+
+#include <iostream>
+
+using namespace std;
+
+int main2()
+{
+    cout << "Namespace yolo!!\n";
+    return 0;
+}
+
+
+/*
+    We use namespace!
+*/
