9b089f94962c06c1258dbbdb03afb40ceefa943d
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-09 20:20:25 +0300

    Create variables01.cpp

---
+++ source_code/variables01.cpp
@@ -0,0 +1,28 @@
+// variables01.cpp : int, char, string
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int birth_year;
+    float num_float;
+    double num_double;
+
+    char ch;
+    string name = "temporary";
+
+    cout << "Write your name: \n";
+    cin >> name;
+
+    cout << "Enter your (real) year of birth: \n";
+    cin >> birth_year;
+
+    cout << "Your name is " << name << " and you are " << 2022 - birth_year << " years old\n";
+    
+    ch = name[0];
+    cout << "Your name's first letter is: " << ch;
+
+    return 0;
+}
